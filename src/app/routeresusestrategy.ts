04391ccc1967c26fd69eb5078883cf5341f6b32c
Sathish Uthayasuriyan <sathishus94@gmail.com>
2017-10-16 10:07:42 +0530

    Create routeresusestrategy.ts

---
+++ src/app/routeresusestrategy.ts
@@ -0,0 +1,41 @@
+/*
+External imports.
+*/
+import { RouteReuseStrategy, ActivatedRouteSnapshot, DetachedRouteHandle } from '@angular/router';
+
+
+export class CustomReuseStrategy implements RouteReuseStrategy { 
+
+  public handlers: {[key: string]: DetachedRouteHandle} = {};
+  
+  constructor() 
+  {
+    
+  }
+
+  shouldDetach(route: ActivatedRouteSnapshot): boolean 
+  {
+    return true;
+  }
+
+  store(route: ActivatedRouteSnapshot, handle: DetachedRouteHandle): void 
+  {
+    this.handlers[route.url.join("/") || route.parent.url.join("/")] = handle;    
+  }
+
+  shouldAttach(route: ActivatedRouteSnapshot): boolean 
+  {
+    return !!this.handlers[route.routeConfig.path];    
+  }
+
+  retrieve(route: ActivatedRouteSnapshot): DetachedRouteHandle 
+  {
+    return this.handlers[route.url.join("/") || route.parent.url.join("/")];    
+  }
+
+  shouldReuseRoute(future: ActivatedRouteSnapshot, curr: ActivatedRouteSnapshot): boolean 
+  {
+    return future.routeConfig === curr.routeConfig;
+  }
+
+}
